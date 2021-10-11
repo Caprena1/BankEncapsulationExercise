54c8e773d22a3964c6e7adf93a78472615252338
Caprena Richie <robcaprena@gmail.com>
2021-10-11 12:27:28 -0400

    first commit

---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bank1 = new BankAccount();
+           
+
+            bank1.Deposit(2.5);
+            var balance = bank1.GetBalance();
+            Console.WriteLine(balance);
+
+        }
+    }
+}
